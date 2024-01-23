94b909907a6562a262b117221a16317de9b3a20d
walloomonero <wallaskhoza@gmail.com>
2024-01-23 20:09:46 +0200

    upate

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,7 +23,7 @@ void swap_ints(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t n, len = size;
+	size_t i, len = size;
 	bool bubb = false;
 
 	if (array == NULL || size < 2)
@@ -32,11 +32,11 @@ void bubble_sort(int *array, size_t size)
 	while (bubb == false)
 	{
 		bubb = true;
-		for (n = 0; n < len - 1; n++)
+		for (i = 0; i < len - 1; i++)
 		{
-			if (array[n] > array[n + 1])
+			if (array[i] > array[i + 1])
 			{
-				swap_ints(array + n, array + n + 1);
+				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
 				bubb = false;
 			}
