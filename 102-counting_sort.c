82d3f4125673075ec75a6e913749ade571a0728e
walloomonero <wallaskhoza@gmail.com>
2024-01-24 01:09:37 +0200

    update

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,36 +1,36 @@
 #include "sort.h"
 
 /**
- * get_max - Get the maximum value in an array of integers.
+ * get_max - To get the maximum value in an array of integers.
  * @array: An array of integers.
- * @size: The size of the array.
+ * @size: Size of the array.
  *
  * Return: The maximum integer in the array.
  */
 int get_max(int *array, int size)
 {
-	int max, i;
+	int maxi, i;
 
-	for (max = array[0], i = 1; i < size; i++)
+	for (maxi = array[0], i = 1; i < size; i++)
 	{
-		if (array[i] > max)
-			max = array[i];
+		if (array[i] > maxi)
+			maxi = array[i];
 	}
 
-	return (max);
+	return (maxi);
 }
 
 /**
- * counting_sort - Sort an array of integers in ascending order
+ * counting_sort - To sort an array of integers in ascending order
  *                 using the counting sort algorithm.
  * @array: An array of integers.
- * @size: The size of the array.
+ * @size: Size of the array.
  *
- * Description: Prints the counting array after setting it up.
+ * Description: To print the counting array after setting it up.
  */
 void counting_sort(int *array, size_t size)
 {
-	int *count, *sorted, max, i;
+	int *count, *sorted, maxi, i;
 
 	if (array == NULL || size < 2)
 		return;
@@ -38,21 +38,21 @@ void counting_sort(int *array, size_t size)
 	sorted = malloc(sizeof(int) * size);
 	if (sorted == NULL)
 		return;
-	max = get_max(array, size);
-	count = malloc(sizeof(int) * (max + 1));
+	maxi = get_max(array, size);
+	count = malloc(sizeof(int) * (maxi + 1));
 	if (count == NULL)
 	{
 		free(sorted);
 		return;
 	}
 
-	for (i = 0; i < (max + 1); i++)
+	for (i = 0; i < (maxi + 1); i++)
 		count[i] = 0;
 	for (i = 0; i < (int)size; i++)
 		count[array[i]] += 1;
-	for (i = 0; i < (max + 1); i++)
+	for (i = 0; i < (maxi + 1); i++)
 		count[i] += count[i - 1];
-	print_array(count, max + 1);
+	print_array(count, maxi + 1);
 
 	for (i = 0; i < (int)size; i++)
 	{
